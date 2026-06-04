d135892f44b394272ef7f65f4ceb4ed02374f099
AnaniyaYosef <yosefananiya@gmail.com>
2026-06-04 07:19:42 +0300

    feat:Employee-dto

---
+++ backend/src/employee/dto/create-employee.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateEmployeeDto {
+  @IsNotEmpty()
+  @IsString()
+  full_name!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  password!: string;
+
+  @IsOptional()
+  phone?: string;
+
+  @IsOptional()
+  role?: string; 
+
+  @IsOptional()
+  department_id?: string;
+
+  @IsOptional()
+  position?: string;
+}
---
+++ backend/src/employee/employee.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+
+@Injectable()
+export class EmployeeService {
+  constructor(private prisma:PrismaService){}
+
+  async create(createEmployeeDto: CreateEmployeeDto) {
+    const emp = await this.prisma.employee.findUnique({
+      where:{email:createEmployeeDto.email}
+    })
+    return 'This action adds a new employee';
+  }
+
+  findAll() {
+    return `This action returns all employee`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} employee`;
+  }
+
+  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    return `This action updates a #${id} employee`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} employee`;
+  }
+}
