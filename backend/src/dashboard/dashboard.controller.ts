2f912276f833d6a3e4aeba0c4c15d95b5a634a53
Diyah <diyahussen23@gmail.com>
2026-06-08 07:45:44 +0300

    dashboard done

--- backend/src/dashboard/dashboard.controller.ts
+++ backend/src/dashboard/dashboard.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) {}
+
+  @Get('admin')
+  getAdminDashboard() {
+    return this.dashboardService.getAdminDashboard();
+  }
+  @Get('employee')
+  getEmployeeDashboard() {
+    return this.dashboardService.getEmployeeDashboard();
+  }
+}
