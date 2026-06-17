f87bcfa3e6978e21d2c84653c681d6d8585b4ad9
amoori.01 <omarjafarcool@gmail.com>
2026-06-17 18:32:28 +0300

    fix(backend): restore missing source files and fix docker healthcheck

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -10,5 +12,7 @@ import { EmployeeModule } from './employee/employee.module';
 
 @Module({
   imports: [PrismaModule, AuthModule, UsersModule, AttendanceModule, LeaveModule, GrievanceModule, DashboardModule, EmployeeModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
