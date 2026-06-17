5a40cf718b182aeb3a67f91772b7784de1f2749e
usman1121 <usmanabdella1121@gmail.com>
2026-06-17 20:20:23 +0300

    Cleaning the authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { AttendanceModule } from './attendance/attendance.module';
-import { LeaveModule } from './leave/leave.module';
-import { GrievanceModule } from './grievance/grievance.module';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, AttendanceModule, LeaveModule, GrievanceModule, DashboardModule, EmployeeModule],
+  imports: [PrismaModule, AuthModule],
 })
 export class AppModule {}
