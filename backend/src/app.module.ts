79b826d6b20042decb0fbbbebca50f95e983aa93
usman1121 <usmanabdella1121@gmail.com>
2026-06-25 15:42:01 +0300

    docker error fixed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,14 +4,14 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { AttendanceModule } from './attendance/attendance.module';
+
 import { LeaveModule } from './leave/leave.module';
 import { GrievanceModule } from './grievance/grievance.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, AttendanceModule, LeaveModule, GrievanceModule, DashboardModule, EmployeeModule],
+  imports: [PrismaModule, AuthModule, UsersModule, LeaveModule, GrievanceModule, DashboardModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
