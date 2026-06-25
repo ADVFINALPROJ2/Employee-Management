83c076b30f02f91729edfd356fa53f34f7ba79d7
AnaniyaYosef <yosefananiya@gmail.com>
2026-06-25 11:15:44 +0300

    dasbord page(emp,admin)

--- backend/src/dashboard/dashboard.controller.ts
+++ backend/src/dashboard/dashboard.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) {}
+
+  @Get('admin')
+  getAdminDashboard() {
+    return this.dashboardService.getAdminDashboard();
+  }
+
+  @Get('employee')
+  getEmployeeDashboard(@Query('employeeId') employeeId: string) {
+    return this.dashboardService.getEmployeeDashboard(employeeId);
+  }
+}
--- backend/src/dashboard/dashboard.service.ts
+++ backend/src/dashboard/dashboard.service.ts
@@ -1,14 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class DashboardService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  async getEmployeeDashboard() {
-    
+  async getEmployeeDashboard(employee_id: string) {
+    const pendingLeaves = await this.prisma.leaveRequest.count({
+      where: {
+        employee_id: employee_id,
+        status: 'Pending',
+      },
+    });
+
+    const approvedLeaves = await this.prisma.leaveRequest.count({
+      where: {
+        employee_id: employee_id,
+        status: 'Approved',
+      },
+    });
+
+    const openGrievances = await this.prisma.grievance.count({
+      where: {
+        employee_id: employee_id,
+        status: 'Under Review',
+      },
+    });
+
+    const leaveBalance = await this.prisma.leaveBalance.findMany({
+      where: {
+        employee_id: employee_id,
+      },
+      select: {
+        leave_type: {
+          select: { name: true },
+        },
+        remaining_days: true,
+      },
+    });
+
+    return {
+      pendingLeaves,
+      approvedLeaves,
+      openGrievances,
+      leaveBalance
+    };
   }
 
-  async  getAdminDashboard(){
+  async getAdminDashboard() {
     const totalEmployees = await this.prisma.employee.count({
       where: {
         status: 'Active',
